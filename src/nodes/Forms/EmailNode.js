0cf75809f14d36331b197b170f840cc323181be7
mka <kayhan.arican@hipporello.com>
2022-06-05 20:08:48 +0300

    check email unique

--- src/nodes/Forms/EmailNode.js
+++ src/nodes/Forms/EmailNode.js
@@ -5,10 +5,16 @@ export default class EmailNode extends AbstractHippoNode{
     super(appJson, path);
   }
   getValidatorFunction() {
+    const formsAndEmails =  this?.appJson?.app?.integrations?.incoming || {};
+    const allEmails = (Object.values(formsAndEmails||{}) || []).filter( e => e?.type === "email" && e?.id !== this?.nodeJson?.id).map(e => e?.email)
     let errors = [];
     if(!this.nodeJson?.enabled)
       return [];
-    else
+    else{
+      if((allEmails||[]).includes(this?.nodeJson?.email)){
+        errors.push({type: "unique", message: "Email must be unique"})
+      }
       return errors
+    }
   }
 }
