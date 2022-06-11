e675555c7ef4b3065a8dc124c7abd14110d1f95c
mka <kayhan.arican@hipporello.com>
2022-06-11 15:18:07 +0300

    1.0.71: page title required fix

--- src/nodes/Views/PageNode.js
+++ src/nodes/Views/PageNode.js
@@ -1,39 +1,41 @@
 import AbstractHippoNode from "../AbstractHippoNode";
 import ChildrenNode from "../Views/ChildrenNode";
 import Validator from "fastest-validator";
-const pageSchema = {
-  enabled: 'boolean',
-  id: 'string',
-  viewProps: {
-    type: 'object',
-    props: {
-      name: 'string',
-      align: {
-        type: 'enum',
-        values: ['left', 'center', 'right'],
-        optional: true
-      },
-      cardAware: 'boolean|optional',
-      children: 'array|optional'
-    }
-  }
-}
-const pageSlugSchema = {
-  viewProps: {
-    type: 'object',
-    props: {
-      slug: 'string',
-    }
-  }
-}
-const pageCheck = new Validator().compile(pageSchema);
-const pageSlugCheck = new Validator().compile(pageSlugSchema);
+
+
 export default class PageNode extends AbstractHippoNode{
   constructor(appJson, path) {
     super(appJson, path);
   }
 
   getValidatorFunction() {
+    const isFormThankYouPage = !this.nodeJson?.viewProps?.environments?.length
+    const pageCheck = new Validator().compile({
+      enabled: 'boolean',
+      id: 'string',
+      viewProps: {
+        type: 'object',
+        props: {
+          name: 'string',
+          align: {
+            type: 'enum',
+            values: ['left', 'center', 'right'],
+            optional: true
+          },
+          cardAware: 'boolean|optional',
+          children: 'array|optional',
+          title: isFormThankYouPage ? "string|optional": "string"
+        }
+      }
+    });
+    const pageSlugCheck = new Validator().compile({
+      viewProps: {
+        type: 'object',
+        props: {
+          slug: 'string',
+        }
+      }
+    });
       let errors = [];
       const pageCheckResult = pageCheck(this.nodeJson);
       if (Array.isArray(pageCheckResult)) {
