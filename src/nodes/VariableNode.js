a99623846043eeacf7d11352c028729ade5d9871
hippo-m <mahmut.isci@hipporello.com>
2023-03-06 17:17:53 +0300

    v.1.0.142 jsonata func ignore

--- src/nodes/VariableNode.js
+++ src/nodes/VariableNode.js
@@ -18,6 +18,9 @@ export default class VariableNode extends AbstractHippoNode {
     let exp = this.ALL_JSONATA_EXPRESSIONS[text];
     if(exp == null){
       exp = jsonata(text);
+      exp.registerFunction("createLinkEnc", () => {}, "<ssssssa:s>")
+      exp.registerFunction("createLink", () => {}, "<ssssssa:s>")
+      exp.registerFunction("toString", () => {}, "<s-a:s>")
       this.ALL_JSONATA_EXPRESSIONS[text] = exp;
     }
     return exp;
@@ -32,16 +35,15 @@ export default class VariableNode extends AbstractHippoNode {
 
   getValidatorFunction() {
     let exp = VariableNode.createJsonataExpression(this.expression)
-    exp.registerFunction("createLinkEnc", (...args) => {}, "<ssssssa:s>")
-    exp.registerFunction("createLink", (...args) => {}, "<ssssssa:s>")
-    exp.registerFunction("toString", function(type, args){}, "<s-a:s>")
     let varErrors = [];
     let staticFields = this.getAccessibleFieldTypes();
     let me = this;
     let activeContext = null;
     let proxy = new Proxy({}, {
       get: function (target, propertyName) {
-        if(varErrors.length === 0 && propertyName !== "sequence" && !me.expression.startsWith("form.")) {
+        const ignoredPropertyNames = ["createLinkEnc", "createLink", "form."];
+        const hasIgnoredPath = ignoredPropertyNames.some(i => me.expression.includes(i))
+        if(varErrors.length === 0 && propertyName !== "sequence" && !hasIgnoredPath) {
           let fieldId = propertyName;
           if (activeContext != null) {
             fieldId = activeContext.resolveBy?activeContext.resolveBy+"."+fieldId:activeContext.id+"."+fieldId
@@ -64,7 +66,14 @@ export default class VariableNode extends AbstractHippoNode {
         return proxy;
       }
     });
-    exp.evaluate(proxy);
+    try{
+      exp.evaluate(proxy);
+    } catch (err){
+      // console.log({me, err})
+    }
+    if(varErrors?.length){
+      console.log(varErrors)
+    }
     return varErrors;
   }
 
