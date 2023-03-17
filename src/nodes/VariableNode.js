152ec174f36a15d71c70263d04cb967fe5874ef5
hippo-m <mahmut.isci@hipporello.com>
2023-03-17 12:55:35 +0300

    fix

--- src/nodes/VariableNode.js
+++ src/nodes/VariableNode.js
@@ -1,26 +1,24 @@
 import AbstractHippoNode from "./AbstractHippoNode";
-import Validator from "fastest-validator";
 import jsonata from "jsonata";
-import * as path from "path";
 
 export default class VariableNode extends AbstractHippoNode {
   static firstLevel = {
-    system:true,
+    system: true,
     portal: true,
     user: true,
-    board:true,
-    card:true,
-    parentCard:true,
-    appParameters:true
-  }
+    board: true,
+    card: true,
+    parentCard: true,
+    appParameters: true,
+  };
   static ALL_JSONATA_EXPRESSIONS = {};
-  static createJsonataExpression(text){
+  static createJsonataExpression(text) {
     let exp = this.ALL_JSONATA_EXPRESSIONS[text];
-    if(exp == null){
+    if (exp == null) {
       exp = jsonata(text);
-      exp.registerFunction("createLinkEnc", () => {}, "<ssssssa:s>")
-      exp.registerFunction("createLink", () => {}, "<ssssssa:s>")
-      exp.registerFunction("toString", () => {}, "<s-a:s>")
+      exp.registerFunction("createLinkEnc", () => {}, "<ssssssa:s>");
+      exp.registerFunction("createLink", () => {}, "<ssssssa:s>");
+      exp.registerFunction("toString", () => {}, "<s-a:s>");
       this.ALL_JSONATA_EXPRESSIONS[text] = exp;
     }
     return exp;
@@ -30,51 +28,59 @@ export default class VariableNode extends AbstractHippoNode {
     this.expression = expression;
   }
 
-  process(appJson, path, nodeJson) {
-  }
+  process(appJson, path, nodeJson) {}
 
   getValidatorFunction() {
-    let exp = VariableNode.createJsonataExpression(this.expression)
+    let exp = VariableNode.createJsonataExpression(this.expression);
     let varErrors = [];
     let staticFields = this.getAccessibleFieldTypes();
     let me = this;
     let activeContext = null;
-    let proxy = new Proxy({}, {
-      get: function (target, propertyName) {
-        const ignoredPropertyNames = ["createLinkEnc", "createLink", "form."];
-        const hasIgnoredPath = ignoredPropertyNames.some(i => me.expression.includes(i))
-        if(varErrors.length === 0 && propertyName !== "sequence" && !hasIgnoredPath) {
-          let fieldId = propertyName;
-          if (activeContext != null) {
-            fieldId = activeContext.resolveBy?activeContext.resolveBy+"."+fieldId:activeContext.id+"."+fieldId
-          }
-          let fieldConfig = staticFields[fieldId];
-          if (!fieldConfig) {
-            varErrors.push({
-              path: me.validatorPath,
-              type: "invalid_variable",
-              message: "Invalid variable field " + propertyName,
-              args: [propertyName, me.expression]
-            })
-          }
-          else{
-            if(activeContext == null || fieldConfig.resolveBy != null){
-              activeContext = fieldConfig;
+    let proxy = new Proxy(
+      {},
+      {
+        get: function (target, propertyName) {
+          const ignoredPropertyNames = ["createLinkEnc", "createLink", "form."];
+          const hasIgnoredPath = ignoredPropertyNames.some((i) =>
+            me.expression.includes(i)
+          );
+          if (
+            varErrors.length === 0 &&
+            propertyName !== "sequence" &&
+            !hasIgnoredPath
+          ) {
+            let fieldId = propertyName;
+            if (activeContext != null) {
+              fieldId = activeContext.resolveBy
+                ? activeContext.resolveBy + "." + fieldId
+                : activeContext.id + "." + fieldId;
+            }
+            let fieldConfig = staticFields[fieldId];
+            if (!fieldConfig) {
+              varErrors.push({
+                path: me.validatorPath,
+                type: "invalid_variable",
+                message: "Invalid variable field " + propertyName,
+                args: [propertyName, me.expression],
+              });
+            } else {
+              if (activeContext == null || fieldConfig.resolveBy != null) {
+                activeContext = fieldConfig;
+              }
             }
           }
-        }
-        return proxy;
+          return proxy;
+        },
       }
-    });
-    try{
+    );
+    try {
       exp.evaluate(proxy);
-    } catch (err){
+    } catch (err) {
       // console.log({me, err})
     }
-    if(varErrors?.length){
-      console.log(varErrors)
+    if (varErrors?.length) {
+      console.log(varErrors);
     }
     return varErrors;
   }
-
 }
